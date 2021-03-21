07327c20f5ae024fad3eaa7173df9be2bdeed569
Patrick Tuszakowski <mrtall500@gmail.com>
2021-03-21 09:13:57 -0400

    test passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    function handleFocus(){
+        console.log('Good!')
+    }
+
+    return (
+        <div>
+            <button onBlur={handleBlur} onFocus={handleFocus} >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+import React, {useState} from 'react'
+
+const Keypad = () => {
+    
+    const [pass, setPass] = useState("")
+    
+    function handlePasswordChange(e){
+        setPass(e.target.value)
+        console.log("Entering password...")
+    }
+    return (
+        <div>
+            <input type="password" value={pass} onChange= {handlePasswordChange} />
+        </div>
+    )
+}
+
+export default Keypad
